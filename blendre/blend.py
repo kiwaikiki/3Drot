77ef4ca6f44b28095119ee7e191b70952c07446f
ViktoriaPravdova <viktoria.pravdova@trojsten.sk>
2024-03-16 21:13:47 +0100

    my version of dataset, network, train

--- blendre/blend.py
+++ blendre/blend.py
@@ -5,107 +5,126 @@ import numpy as np
 from io import StringIO   # StringIO behaves like a file object
 import time
 import pandas as pd
+import multiprocessing as mp
 
 PROJECT_DIR = '/home/viki/FMFI/bc/blendre/'
 os.chdir(PROJECT_DIR)
 
-def render_object_from_angles(obj, output_dir, matrix_file, number_of_angles=100):
+class PictureGenerator:
+    def __init__(self, obj_file : str,  render_x_y : tuple, texture_file : str = None) -> None:
+        self.cleanup_scene()
+        self.set_scene()
+        self.set_rendering_settings(*render_x_y)
+        self.obj = self.import_object(obj_file)
+        if texture_file:
+            self.add_texture(self.obj, texture_file)  
 
-    with open(matrix_file, 'w') as f:
-        start_time = time.time()  
-        for id, angle in enumerate(generate_uniform_angles(number_of_angles)):
-            obj.rotation_euler = angle
-            if matrix_file.endswith('.csv'):
-                matrix = matrix2csv(obj.matrix_world)
-                print(f'{id},{matrix}', file = f)
-            else:  
-                matrix = matrix2string(obj.matrix_world) 
-                print(f'{id}\n{matrix}', file = f)
-            bpy.context.scene.render.filepath = os.path.join(output_dir, f"{id}.png")
-            bpy.ops.render.render(write_still=True)
-    
-        print(f'Elapsed time: {time.time() - start_time}')
+    def cleanup_scene(self):
+        bpy.ops.object.select_all(action='SELECT')
+        bpy.ops.object.delete()
+        bpy.ops.object.select_all(action='DESELECT')
 
-def matrix2string(matrix):
-    '''
-    returns string parsable by numpy. 
-    discards last row and column(translation).
-    '''
-    return '\n'.join([' '.join(map(str, row[:-1])) for row in matrix[:-1]])   
+    def set_scene(self):
+        '''
+        add light and camera
+        '''
+        bpy.ops.object.light_add(type='POINT', location=(5, -10, 5))
+        light = bpy.context.object
+        light.data.energy = 5000
+        bpy.ops.object.camera_add(location=(0, -15, 0))
+        bpy.data.objects['Camera'].rotation_euler = (1.5708, 0, 0)  # Rotate camera to point downwards
+        bpy.context.scene.camera = bpy.context.object
 
-def matrix2csv(matrix):
-    '''
-    returns string parsable by numpy. 
-    discards last row and column(translation).
-    order first row, second row
-    '''
-    return ','.join([','.join(map(str, row[:-1])) for row in matrix[:-1]])
+    def render_object_from_angles(self, obj, output_dir, matrix_file, number_of_angles=100):
+        with open(matrix_file, 'w') as f:
+            start_time = time.time()  
+            for id, angle in enumerate(self.generate_uniform_angles(number_of_angles)):
+                obj.rotation_euler = angle
+                if matrix_file.endswith('.csv'):
+                    matrix = self.matrix2csv(obj.matrix_world)
+                    print(f'{id},{matrix}', file = f)
+                else:  
+                    matrix = self.matrix2string(obj.matrix_world) 
+                    print(f'{id}\n{matrix}', file = f)
+                bpy.context.scene.render.filepath = os.path.join(output_dir, f"{id}.png")
+                bpy.ops.render.render(write_still=True)
+        
+            print(f'Elapsed time: {time.time() - start_time}')
 
-def cleanup_scene():
-    bpy.ops.object.select_all(action='SELECT')
-    bpy.ops.object.delete()
-    bpy.ops.object.select_all(action='DESELECT')
+    def matrix2string(self, matrix):
+        '''
+        returns string parsable by numpy. 
+        discards last row and column(translation).
+        '''
+        return '\n'.join([' '.join(map(str, row[:-1])) for row in matrix[:-1]])   
 
-def add_texture(ob):
-    '''
-    object should already have uv map
-    '''
-    mat = bpy.data.materials.new(name="New_Mat")
-    mat.use_nodes = True
-    bsdf = mat.node_tree.nodes["Principled BSDF"]
-    texImage = mat.node_tree.nodes.new('ShaderNodeTexImage')
-    texImage.image = bpy.data.images.load('textury/voronoi.png')
-#    texImage.image = bpy.data.images.load('textury/gray_rocks_4k.blend/textures/gray_rocks_nor_gl_4k.exr')
-    mat.node_tree.links.new(bsdf.inputs['Base Color'], texImage.outputs['Color'])
-    if ob.data.materials:
-        ob.data.materials[0] = mat
-    else:
-        ob.data.materials.append(mat)
+    def matrix2csv(self, matrix):
+        '''
+        returns string parsable by numpy. 
+        discards last row and column(translation).
+        order first row, second row
+        '''
+        return ','.join([','.join(map(str, row[:-1])) for row in matrix[:-1]])
 
-def set_scene():
-    '''
-    add light and camera
-    '''
-    bpy.ops.object.light_add(type='POINT', location=(5, -10, 5))
-    light = bpy.context.object
-    light.data.energy = 5000
-    bpy.ops.object.camera_add(location=(0, -15, 0))
-    bpy.data.objects['Camera'].rotation_euler = (1.5708, 0, 0)  # Rotate camera to point downwards
-    bpy.context.scene.camera = bpy.context.object
+    def add_texture(self, obj, texture_file=None):
+        '''
+        object should already have uv map
+        '''
+        mat = bpy.data.materials.new(name="New_Mat")
+        mat.use_nodes = True
+        bsdf = mat.node_tree.nodes["Principled BSDF"]
+        texImage = mat.node_tree.nodes.new('ShaderNodeTexImage')
+        texImage.image = bpy.data.images.load(texture_file)
+    #    texImage.image = bpy.data.images.load('textury/gray_rocks_4k.blend/textures/gray_rocks_nor_gl_4k.exr')
+        mat.node_tree.links.new(bsdf.inputs['Base Color'], texImage.outputs['Color'])
+        if obj.data.materials:
+            obj.data.materials[0] = mat
+        else:
+            obj.data.materials.append(mat)
 
-def import_object(file_path):
-    print(bpy.ops.import_scene.fbx(filepath='modely/kocky_texture.fbx'))
-    # bpy.ops.import_scene.obj(filepath=file_path)
-    bpy.ops.object.select_all(action='SELECT')
-    obj = bpy.context.selected_objects[2] # 0 is camera, 1 is light
-    return obj
+    def import_object(self, ile_path):
+        print(bpy.ops.import_scene.fbx(filepath='modely/kocky_texture.fbx'))
+        # bpy.ops.import_scene.obj(filepath=file_path)
+        bpy.ops.object.select_all(action='SELECT')
+        obj = bpy.context.selected_objects[-1] # 0 is camera, 1 is light
+        return obj
 
-def set_rendering_settings(x, y):
-    bpy.context.scene.render.resolution_x = x
-    bpy.context.scene.render.resolution_y = y
+    def set_rendering_settings(self, x, y):
+        bpy.context.scene.render.resolution_x = x
+        bpy.context.scene.render.resolution_y = y
 
-def generate_uniform_angles(n):
-    for _ in range(n):
-        yield (np.random.uniform(0, 2*np.pi), 
-               np.random.uniform(0, 2*np.pi), 
-               np.random.uniform(0, 2*np.pi))
+    def generate_uniform_angles(self, n):
+        for _ in range(n):
+            yield (np.random.uniform(0, 2*np.pi), 
+                np.random.uniform(0, 2*np.pi), 
+                np.random.uniform(0, 2*np.pi))
 
-def get_matrix_from_csv(id, table):
-    return np.array(table.loc[id]).reshape(3, 3)
+    def get_matrix_from_csv(self, id, table):
+        return np.array(table.loc[id]).reshape(3, 3)
 
-def generate():
-    cleanup_scene()
-    set_scene()
-    obj = import_object("modely/kocky_vstrede.obj")
-    add_texture(obj)
-    set_rendering_settings(400, 400)
-    render_object_from_angles(obj, "output", 'matice.csv', 10)
-    return obj
+    def paralel_rendering(self, n=100):
+        with mp.Pool(10) as p:
+            p.map(help_paralel, zip(range(n), [self.obj.copy() for _ in range(n)]))
+
+def help_paralel(args):
+    obj_id, obj = args
+    obj.rotation_euler = (np.random.uniform(0, 2*np.pi), 
+                        np.random.uniform(0, 2*np.pi), 
+                        np.random.uniform(0, 2*np.pi))
+    bpy.context.scene.render.filepath = os.path.join('output', f"{obj_id}.png")
+    bpy.ops.render.render(write_still=True)
+    with open('matice.csv', 'w') as f:
+        matrix = PictureGenerator.matrix2csv(obj.matrix_world)
+        print(f'{obj_id},{matrix}', file = f)
+        
 
 
 if __name__ == "__main__":
-    obj = generate()
 
+    gen = PictureGenerator('modely/kocky_texture.fbx', (400, 400), 'textury/voronoi.png')
+    
+    gen.render_object_from_angles(gen.obj, 'output', 'matice.csv', 10000)
+    # gen.paralel_rendering()
 # check
     with open('matice.txt', 'r') as f:
         l = ''.join(f.readlines()[1:4])
@@ -116,11 +135,11 @@ if __name__ == "__main__":
     
     with open('matice.csv', 'r') as f:
         table = pd.read_csv(f, header=None, index_col=0)
-        M = get_matrix_from_csv(2, table)
+        M = PictureGenerator.get_matrix_from_csv(2, table)
         print(M)
         print(np.linalg.det(M))
         print(M@M.T)
-        
+            
 
 
 
--- blendre/my_dataset.py
+++ blendre/my_dataset.py
@@ -1,27 +1,36 @@
-from torch.utils.data import Dataset
+from torch.utils.data import Dataset, DataLoader
 import numpy as np
 import pandas as pd
 import cv2
 import os
-
+import torch    
+# from matplotlib import pyplot as plt
 class Dataset(Dataset):
-    def __init__(self, path_pics : str, path_csv : str):
+    def __init__(self, path_pics : str, path_csv : str, split = 'train', width = 400, height = 400):
         self.path_pics = path_pics
         self.path_csv = path_csv
-        self.width = 400
-        self.height = 400
-        self.matrices = None
-
+        self.split = split
+        self.width = width
+        self.height = height
+        self.entries = None
         self.load(path_csv)
+
+        if self.split == 'train':
+            self.entries = [entry for i, entry in enumerate(self.entries) if i % 5 != 0]
+        elif self.split == 'val':
+            self.entries = [entry for i, entry in enumerate(self.entries) if i % 5 == 0]
+
+        print("Split: ", self.split)
+        print("Size: ", len(self))
+        
 
     def load(self, path):
         table = pd.read_csv(path)
-        def help_func(x):
+        def help2matrix(x):
             return np.array([[x[1], x[2], x[3]],
                               [x[4], x[5], x[6]],
                               [x[7], x[8], x[9]]])
-        self.matrices = np.apply_along_axis(help_func, arr = table, axis = 1)
-
+        self.entries = np.apply_along_axis(help2matrix, arr = table, axis = 1)
 
     def load_xyz(self, id_pic : int):
         """
@@ -34,19 +43,52 @@ class Dataset(Dataset):
         if xyz is None:
             print(exr_path)
             raise ValueError("Image at path ", exr_path)
-        xyz = self.add_sur(xyz)
+        xyz = cv2.resize(xyz, (self.width, self.height), cv2.INTER_NEAREST_EXACT)
+        xyz = np.transpose(xyz, [2, 0, 1])
+        # xyz = self.add_sur_2(xyz)
         return xyz
     
     def add_sur(self, xyz):
         h_sur, w_sur = np.indices((self.height, self.width))
-        xyz = np.dstack((xyz, h_sur))
-        xyz = np.dstack((xyz, w_sur))
+        xyz = np.dstack((xyz, h_sur, w_sur))
+        print(xyz.shape)
+        return xyz
+    
+    def add_sur_2(self, xyz):
+        h_sur, w_sur = np.indices((self.height, self.width))
+        r = xyz[:,:,0]
+        g = xyz[:,:,1]
+        b = xyz[:,:,2]
+        # xyz = np.array([r, g, b, h_sur, w_sur])
+        xyz = np.array([r, g, b])
         return xyz
 
+    def __len__(self):
+        return len(self.entries)
     
+    def __getitem__(self, index):
+        xyz = self.load_xyz(index)
+        xyz = xyz.astype(np.float32)
+
+        return {'id': index, 
+                'bin_transform': torch.from_numpy(self.entries[index]), 
+                'xyz' : xyz}
+
 
 if __name__ == "__main__":
     pic_dir = '/home/viki/FMFI/bc/blendre/output/'
     csv_dir = '/home/viki/FMFI/bc/blendre/matice.csv'
     dataset = Dataset(pic_dir, csv_dir)
-    dataset.load_xyz(5)
+    data_loader = DataLoader(dataset, batch_size=4, shuffle=True, num_workers=1)
+
+
+    for item in data_loader:
+        # print(item['xyz'].size())
+        xyz = item['xyz'][0].cpu().detach().numpy()
+        print(np.mean(xyz))
+
+        # fig = plt.figure()
+        # ax = fig.add_subplot(projection='3d')
+        # ax.scatter(xyz[0].ravel(), xyz[1].ravel(), xyz[2].ravel(), marker='o')
+
+        # plt.show ()
