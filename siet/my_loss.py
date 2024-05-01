c6a3bb25923609a9a48088a5984c62d72840b5ce
Viktória Pravdová <viktoria.pravdova@trojsten.sk>
2024-05-01 21:42:56 +0200

    parametrizovane ktore loss f + dedicnost

--- siet/my_loss.py
+++ siet/my_loss.py
@@ -20,7 +20,9 @@ def get_angles(pred, gt, sym_inv=False, eps=1e-7):
     else:
         angles = torch.acos(torch.clamp(dot/(eps + pred_norm * gt_norm), -1 + eps, 1 - eps))
     return angles
+
 # kedy get_angles kedy angle
+
 def angle(x, y):
     '''
     Calculate the angle between two vectors x and y
@@ -43,19 +45,84 @@ def calculate_eTE(R_gt, R_est):
     angles = torch.stack([rotation_angle(R_gt[i].T @ R_est[i]) for i in range(len(R_gt))])
     return torch.mean(angles).cuda()
 
+def angles2Rotation_Matrix(angles):
+    '''
+    Convert angles to a rotation matrix
+    '''
+    x, y, z = angles
+    Rx = torch.tensor([[1, 0, 0], [0, torch.cos(x), -torch.sin(x)], [0, torch.sin(x), torch.cos(x)]])
+    Ry = torch.tensor([[torch.cos(y), 0, torch.sin(y)], [0, 1, 0], [-torch.sin(y), 0, torch.cos(y)]])
+    Rz = torch.tensor([[torch.cos(z), -torch.sin(z), 0], [torch.sin(z), torch.cos(z), 0], [0, 0, 1]])
+    return Rz @ Ry @ Rx
 
-class GSLossCalculator:
-    def __init__(self):
+def angles2Rotation_Matrix_for_batches(angles):
+    '''
+    Convert batch of angles to a rotation matrices
+    '''
+    # angles is a tensor of shape (batch_size, 3)
+    x, y, z = angles[:, 0], angles[:, 1], angles[:, 2]
+    Rx = torch.stack([torch.stack([torch.ones_like(x), torch.zeros_like(x), torch.zeros_like(x)], dim=-1),
+                      torch.stack([torch.zeros_like(x), torch.cos(x), -torch.sin(x)], dim=-1),
+                      torch.stack([torch.zeros_like(x), torch.sin(x), torch.cos(x)], dim=-1)], dim=-1)
+    Ry = torch.stack([torch.stack([torch.cos(y), torch.zeros_like(y), torch.sin(y)], dim=-1),
+                      torch.stack([torch.zeros_like(y), torch.ones_like(y), torch.zeros_like(y)], dim=-1),
+                      torch.stack([-torch.sin(y), torch.zeros_like(y), torch.cos(y)], dim=-1)], dim=-1)
+    Rz = torch.stack([torch.stack([torch.cos(z), -torch.sin(z), torch.zeros_like(z)], dim=-1),
+                      torch.stack([torch.sin(z), torch.cos(z), torch.zeros_like(z)], dim=-1),
+                      torch.stack([torch.zeros_like(z), torch.zeros_like(z), torch.ones_like(z)], dim=-1)], dim=-1)
+    return torch.matmul(Rz, torch.matmul(Ry, Rx))
+
+def rotation_Matrix2angles(R):
+    '''
+    Convert a rotation matrix to angles
+    '''
+    x = torch.arctan2(R[2, 1], R[2, 2])
+    y = torch.arctan2(-R[2, 0], torch.sqrt(R[2, 1]**2 + R[2, 2]**2))
+    z = torch.arctan2(R[1, 0], R[0, 0])
+    return torch.tensor([x, y, z])
+
+
+class LossCalculator:
+    def __init__(self, loss_type='angle'):
+        self.loss_type = loss_type
+
         self.loss_running = torch.from_numpy(np.array([0], dtype=np.float32)).cuda()
-        self.loss_z_running = torch.from_numpy(np.array([0], dtype=np.float32)).cuda()
-        self.loss_y_running = torch.from_numpy(np.array([0], dtype=np.float32)).cuda()
         
         self.train_loss_all = []
         self.val_loss_all = []
 
         self.val_losses = []
+
+    def reset_val_loss(self):
+        self.val_losses = []
+
+    def calculate_train_loss(self, preds, true_transform):
+        return self.function_dict[self.loss_type][0](preds, true_transform)
+    
+    def calculate_val_loss(self, preds, true_transform):
+        return self.function_dict[self.loss_type][1](preds, true_transform)
+
+    def append_to_train_loss_all(self):
+        self.train_loss_all.append(self.loss_running.detach().item())
+
+    def append_to_val_loss_all(self):
+        self.val_loss_all.append(torch.mean(self.val_losses))
+
+    
+
+class GSLossCalculator(LossCalculator):
+    def __init__(self, loss_type='angle'):
+        super().__init__(loss_type)
+        self.loss_z_running = torch.from_numpy(np.array([0], dtype=np.float32)).cuda()
+        self.loss_y_running = torch.from_numpy(np.array([0], dtype=np.float32)).cuda()
+        
         self.val_losses_z = []
         self.val_losses_y = []
+
+        self.function_dict = {'angle': (self.calculate_angle_loss, 
+                                    self.calculate_val_angle_loss),
+                        'elements': (self.calculate_elements_train_loss,
+                                    self.calculate_elements_val_loss)}
 
         self.folder = 'results/GS'
         self.train_file = self.folder + 'train_err2.out'
@@ -63,8 +130,8 @@ class GSLossCalculator:
         # self.all_running = self.folder + 'train_err_running2.out'
         # self.just_ends_file = self.folder + 'train_err_just_ends2.out'
 
-    @staticmethod
-    def calculate_angle_loss(pred_z, pred_y, true_transform):
+    def calculate_angle_loss(self, preds, true_transform):
+        pred_z, pred_y = preds
         z = pred_z
         z = z / torch.linalg.norm(z)
         
@@ -82,14 +149,23 @@ class GSLossCalculator:
 
         # retype true trans from double to float
         true_transfrm = true_transform.float()
-        loss = calculate_eTE(true_transfrm, transform.cuda())
-        print(f'Loss: {loss}')
+        loss = torch.mean(calculate_eTE(true_transfrm, transform.cuda()))
+
+        self.loss_running = 0.9 * self.loss_running + 0.1 * loss
+
+        print(f'Running Loss: {self.loss_running.item()}')
         return loss
     
-    def get_val_angle_loss(self, pred_z, pred_y, true_transform):
-        self.val_losses.append(self.calculate_angle_loss_GS(pred_z, pred_y, true_transform).detach().item())
+    def calculate_val_angle_loss(self, preds, true_transform):
+        self.val_losses.append(self.calculate_angle_loss(preds, true_transform).detach().item())
 
-    def calculate_train_loss(self, pred_z, pred_y, transform): 
+    def reset_val_loss(self):
+        self.val_losses = []
+        self.val_losses_z = []
+        self.val_losses_y = []
+
+    def calculate_elements_train_loss(self, preds, transform): 
+        pred_z, pred_y = preds
         loss_z = torch.mean(get_angles(pred_z, transform[:, :3, 2].cuda()))
         loss_y = torch.mean(get_angles(pred_y, transform[:, :3, 1].cuda()))
         loss = loss_z + loss_y  
@@ -102,10 +178,8 @@ class GSLossCalculator:
         self.print_running_loss()
         return loss
     
-    def append_to_train_loss_all(self):
-        self.train_loss_all.append(self.loss_running.detach().item())
-
-    def calculate_val_loss(self, pred_z, pred_y, transform): 
+    def calculate_elements_val_loss(self, preds, transform): 
+        pred_z, pred_y = preds
         loss_z = torch.mean(get_angles(pred_z, transform[:, :3, 2].cuda()))
         loss_y = torch.mean(get_angles(pred_y, transform[:, :3, 1].cuda(), sym_inv=True))
         loss = loss_z + loss_y 
@@ -114,9 +188,7 @@ class GSLossCalculator:
         self.val_losses_z.append(loss_z.detach().item())
         self.val_losses_y.append(loss_y.detach().item())
 
-    def append_to_val_loss_all(self):
-        self.val_loss_all.append(np.mean(self.val_losses))
-    
+   
     def print_running_loss(self):
         # with open(self.all_running, 'a') as f:
         #     print("Running loss: {}, z loss: {}, y loss: {}"
@@ -146,19 +218,54 @@ class GSLossCalculator:
         np.savetxt(self.val_file, self.val_loss_all, delimiter=',')
 
 
-class EulerLossCalculator:
-    def __init__(self):
-        self.loss_running = torch.from_numpy(np.array([0], dtype=np.float32)).cuda()
-        
-        self.train_loss_all = []
-        self.val_loss_all = []
+class EulerLossCalculator(LossCalculator):
+    def __init__(self, loss_type='angle'):
+        super().__init__(loss_type)
 
-        self.val_losses = []
+        self.function_dict = {'angle': (self.calculate_angle_loss, 
+                                    self.calculate_val_angle_loss),
+                        'elements': (self.calculate_elements_loss,
+                                    self.calculate_val_elements_loss)}
 
-        self.folder = 'results/Euler'
+        self.folder = f'results/Euler/{loss_type}/'
         self.train_file = self.folder + 'train_err2.out'
         self.val_file = self.folder + 'val_err2.out'
-    
+
+
     def calculate_angle_loss(self, pred, gt):
+        pred_R = angles2Rotation_Matrix_for_batches(pred)
+        true_transfrm = gt.float()
+
+        loss = torch.mean(calculate_eTE(true_transfrm, pred_R))
+        self.loss_running = 0.9 * self.loss_running + 0.1 * loss
+
+        print(f'Running Loss: {self.loss_running.item()}')
+        return loss
+    
+    def calculate_val_angle_loss(self, pred, gt):
+        self.val_losses.append(self.calculate_angle_loss(pred, gt).detach().item())
+           
+    
+    def calculate_elements_loss(self, preds, true_transform):
+        gt_angles = torch.stack([rotation_Matrix2angles(true_transform[i]) for i in range(len(true_transform))])
+
+        loss = torch.mean(get_angles(preds, gt_angles))
+        self.loss_running = 0.9 * self.loss_running + 0.1 * loss
+
+        print(f'Running Loss: {self.loss_running.item()}')
+        return loss
+    
+    def calculate_val_elements_loss(self, preds, true_transform):
+        pass
 
     
+    def print_results(self, e, epochs):
+        print(20 * "*")
+        print("Epoch {}/{}".format(e, epochs))
+        print("mean - val loss: {}".format(np.mean(self.val_losses)))
+        print("median - val loss: {}".format(np.median(self.val_losses)))
+
+    def save_results(self):
+        np.set_printoptions(suppress=True)
+        np.savetxt(self.train_file, self.train_loss_all, delimiter=',')
+        np.savetxt(self.val_file, self.val_loss_all, delimiter=',')
