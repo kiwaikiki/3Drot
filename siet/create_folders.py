e4b24fdc15d0544bebe5b330c0124997d87d838e
Viktória Pravdová <viktoria.pravdova@trojsten.sk>
2024-05-06 14:38:59 +0200

    Add script to create folders for checkpoints, results, and inferences

---
+++ siet/create_folders.py
@@ -0,0 +1,47 @@
+import os
+reprs = [
+        'GS',
+        'Euler',
+        'Euler_binned',
+        'Quaternion',
+        'Axis_Angle',
+        'Axis_Angle_binned',
+        'Stereographic'
+    ]
+losses = ['angle', 'elements']
+
+directory = 'bc/siet/'
+# create folders in checkpoints
+for r in reprs:
+    path = os.path.join(directory, 'checkpoints', r)
+    if not os.path.exists(path):
+        os.makedirs(path)
+    for l in losses:
+        path = os.path.join(directory, 'checkpoints', r, l)
+        if not os.path.exists(path):
+            os.makedirs(path)
+            if os.path.exists(path):
+                print('done ', path)
+   
+    # create folders in results
+    path = os.path.join(directory, 'results', r)
+    if not os.path.exists(path):
+        os.makedirs(path)
+    for l in losses:
+        path = os.path.join(directory, 'results', r, l)
+        if not os.path.exists(path):
+            os.makedirs(path)
+            if os.path.exists(path):
+                print('done ', path)
+
+    # create folders in inferences
+    path = os.path.join(directory, 'inferences')
+    if not os.path.exists(path):
+        os.makedirs(path)
+    for l in losses:
+        path = os.path.join(directory, 'inferences', r, l)
+        if not os.path.exists(path):
+            os.makedirs(path)
+            if os.path.exists(path):
+                print('done ', path)
+
